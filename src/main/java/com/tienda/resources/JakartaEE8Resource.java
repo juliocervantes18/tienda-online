3b405779654804f1452908c1b345d9db16049b40
Julio Cervantes <julio18.cervantes@gmail.com>
2025-12-03 04:17:43 -0700

    Subir proyecto inicial

---
+++ src/main/java/com/tienda/resources/JakartaEE8Resource.java
@@ -0,0 +1,21 @@
+package com.tienda.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping() {
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
+
